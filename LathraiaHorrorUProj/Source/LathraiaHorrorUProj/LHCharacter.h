3a473b1e2c2195062ab9291dd050d25d98061b36
Justin Green <justingreen.business@gmail.com>
2024-09-06 15:19:38 +1000

    Add(LHCharacter Class) : Created a custom character class using custom inputs

---
+++ LathraiaHorrorUProj/Source/LathraiaHorrorUProj/LHCharacter.h
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "LHCharacter.generated.h"
+
+class UInputComponent;
+class USkeletalMeshComponent;
+class USceneComponent;
+class UCameraComponent;
+class UAnimMontage;
+class USoundBase;
+class UInputConfig;
+struct FInputActionValue;
+
+// Declaration of the delegate that will be called when the Primary Action is triggered
+// It is declared as dynamic so it can be accessed also in Blueprints
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnInteract);
+
+UCLASS(config = Game)
+class LATHRAIAHORRORUPROJ_API ALHCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	/** Pawn mesh: 1st person view (arms; seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USkeletalMeshComponent* Mesh1P;
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FirstPersonCameraComponent;
+
+public:
+	ALHCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float TurnRateGamepad;
+
+	/** Delegate to whom anyone can subscribe to receive this event */
+	UPROPERTY(BlueprintAssignable, Category = "Interaction")
+	FOnInteract OnInteract;
+
+protected:
+
+	/** Checks for interaction. */
+	void OnPrimaryAction();
+
+	/**
+	 * Called via input to turn at a given rate.
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void TurnAtRate(float Rate);
+
+	/**
+	 * Called via input to turn look up/down at a given rate.
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void LookUpAtRate(float Rate);
+
+protected:
+	// APawn interface
+	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+	// End of APawn interface
+
+public:
+	/** Returns Mesh1P subobject **/
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	/** Returns FirstPersonCameraComponent subobject **/
+	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
+
+	// Begin Enhanced Input Sample changes
+public:
+
+	/** The input config that maps Input Actions to Input Tags*/
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputConfig* InputConfig;
+
+	/** Handles moving forward/backward */
+	void Input_Move(const FInputActionValue& InputActionValue);
+
+	/** Handles mouse and stick look */
+	void Input_Look(const FInputActionValue& InputActionValue);
+
+	/** Handles Jumping */
+	void Input_Jump(const FInputActionValue& InputActionValue);
+
+	/** Handles Interaction */
+	void Input_Interact(const FInputActionValue& InputActionValue);
+
+	// End Enhanced Input Sample changes
+};
